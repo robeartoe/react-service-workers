a57d6ced77cdf8ff5c9a31bfb31e2d598674c143
Robert Ruiz <rsblooxpert@gmail.com>
2020-10-12 18:44:47 -0700

    TODO - Implement Fibonacci sequence

--- src/containers/default/index.tsx
+++ src/containers/default/index.tsx
@@ -1,7 +1,32 @@
-import { Grid } from '@material-ui/core'
-import React from 'react'
+import { Button, CircularProgress, Grid, TextField, Typography } from '@material-ui/core'
+import React, { useState } from 'react'
+
+const centerGrid = {
+    position: "absolute",
+    left: '50%',
+    top: '50%',
+    WebkitTransform: 'translate(-50%, -50%)',
+    transform: 'translate(-50%, -50%)'
+} as React.CSSProperties
+
+const centerItem = {
+    paddingTop: '0.5rem',
+    paddingBottom: '0.5rem'
+};
 
 export default function DefaultTab() {
+    const [value, setValue] = useState(5);
+    const [loading, setLoading] = useState(true);
+    const [fibonacciValue, setFibonacciValue] = useState<number| undefined>(undefined);
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setValue(+event.target.value);
+    };
+
+    const handleClick = () => {
+        setLoading(true);
+        //  TODO: Implement Fibonacci Sequence:
+    }
+
     return (
         <Grid 
             container 
@@ -10,8 +35,32 @@ export default function DefaultTab() {
             alignItems="center"
         >
             <Grid xs={6} item>
-                Default Tab:
+                <Typography>
+                    This section is demonstating a react application with no service workers. It calculates the fibonacci number in this same thread. This results in the main event loop severely being affected.
+                    You can view this by comparing the loading "spinning animation" between this implementation & the default one.
+                </Typography>
             </Grid>
+            <Grid container style={centerGrid} direction="column"
+            justify="center"
+            alignItems="center">
+                <Grid xs={6} item style={centerItem}>
+                    <TextField value={value} onChange={handleChange} fullWidth/>
+                </Grid>
+                <Grid xs={6} item style={centerItem}>
+                    <Button onClick={handleClick}  variant="contained">
+                        Calculate Fibonacci!
+                    </Button>
+                </Grid>
+                <Grid xs={6} item style={centerItem}>
+                    <Typography>
+                        The Fibonacci value is: {fibonacciValue}
+                    </Typography>
+                </Grid>
+                <Grid xs={6} item style={centerItem}>
+                    {loading ? <CircularProgress /> : null}
+                </Grid>
+            </Grid>
+            
         </Grid>
     )
 }
--- src/containers/service-worker/index.tsx
+++ src/containers/service-worker/index.tsx
@@ -1,7 +1,33 @@
-import { Button, Grid, TextField } from '@material-ui/core'
-import React from 'react'
+import React, { useState } from 'react'
+import { Button, CircularProgress, Grid, TextField, Typography } from '@material-ui/core'
+
+const centerGrid = {
+    position: "absolute",
+    left: '50%',
+    top: '50%',
+    WebkitTransform: 'translate(-50%, -50%)',
+    transform: 'translate(-50%, -50%)'
+} as React.CSSProperties
+
+const centerItem = {
+    paddingTop: '0.5rem',
+    paddingBottom: '0.5rem'
+};
 
 export default function ServiceWorkerTab() {
+    const [value, setValue] = useState(5);
+    const [loading, setLoading] = useState(true);
+    const [fibonacciValue, setFibonacciValue] = useState<number| undefined>(undefined);
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setValue(+event.target.value);
+    };
+
+    const handleClick = () => {
+        setLoading(true);
+        // TODO: Implement Fibonacci Sequence:
+        // TODO: Await Service Worker Implementation
+    }
+
     return (
         <Grid 
             container 
@@ -10,9 +36,32 @@ export default function ServiceWorkerTab() {
             alignItems="center"
         >
             <Grid xs={6} item>
-                <TextField />
-                <Button />
+                <Typography>
+                    This section is demonstating service workers. It offloads the fibonacci calculation in another thread. This results in the main event loop not being affected.
+                    You can view this by comparing the loading "spinning animation" between this implementation & the default one.
+                </Typography>
             </Grid>
+            <Grid container style={centerGrid} direction="column"
+            justify="center"
+            alignItems="center">
+                <Grid xs={6} item style={centerItem}>
+                    <TextField value={value} onChange={handleChange} fullWidth/>
+                </Grid>
+                <Grid xs={6} item style={centerItem}>
+                    <Button onClick={handleClick}  variant="contained">
+                        Calculate Fibonacci!
+                    </Button>
+                </Grid>
+                <Grid xs={6} item style={centerItem}>
+                    <Typography>
+                        The Fibonacci value is: {fibonacciValue}
+                    </Typography>
+                </Grid>
+                <Grid xs={6} item style={centerItem}>
+                    {loading ? <CircularProgress /> : null}
+                </Grid>
+            </Grid>
+            
         </Grid>
     )
 }
